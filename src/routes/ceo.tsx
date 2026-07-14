84ba88fa8caa5e43c6e6c3affde980b5b35e4b3d
truhubsolutions16 <truhubsolutions16@gmail.com>
2026-07-14 06:21:09 +0530

    Update ceo.tsx

--- src/routes/ceo.tsx
+++ src/routes/ceo.tsx
@@ -3,22 +3,30 @@ import { CEO } from "@/components/site/ceo";
 
 export const Route = createFileRoute("/ceo")({
   head: () => ({
-    meta: [
-      {
-        title: "CEO | Bharani Kumar.G | TruHub Solutions",
-      },
-      {
-        name: "description",
-        content:
-          "Meet Bharani Kumar.G, CEO of TruHub Solutions. Discover his leadership and business strategy behind the company's growth.",
-      },
-      {
-        name: "keywords",
-        content:
-          "CEO, Bharani Kumar.G, TruHub Solutions, Technology CEO, Business Leader",
-      },
-    ],
-  }),
+  links: [
+  {
+    rel: "canonical",
+    href: "https://truhubsolutions.in/ceo",
+  },
+],
+meta: [
+  {
+    property: "og:title",
+    content: "CEO | Bharani Kumar.G | TruHub Solutions",
+  },
+  {
+    property: "og:description",
+    content: "Meet the CEO of TruHub Solutions.",
+  },
+  {
+    property: "og:image",
+    content: "https://truhubsolutions.in/ceo-truhub.webp",
+  },
+  {
+    property: "og:url",
+    content: "https://truhubsolutions.in/ceo",
+  },
+]
   component: CEOPage,
 });
 
